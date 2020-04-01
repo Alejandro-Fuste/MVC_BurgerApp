b06043b6e92c7c7e3dfae07c93b34f59a61fe894
Alejandro F <afuste7@gmail.com>
2020-03-31 23:02:24 -0400

    add code to schema file and values to seeds file.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', false);
+INSERT INTO burgers (burger_name) VALUES ('turkeyburger');
+INSERT INTO burgers (burger_name) VALUES ('bleu cheese burger');
