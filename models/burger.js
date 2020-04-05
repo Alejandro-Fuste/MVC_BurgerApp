69361d872e005a9466735277b3edae55f8ca27cf
Alejandro F <afuste7@gmail.com>
2020-04-05 19:29:15 -0400

    added code to burger js file

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+// import orm
+const orm = require('../config/orm');
+
+// code that calls the ORM functions
+
+const table = 'burgers';
+
+const burger = {
+	all: function(cb) {
+		orm.selectAll(table, function(res) {
+			cb(res);
+		});
+	},
+	create: function(b_name, dev, cb) {
+		orm.insertOne(table, b_name, dev, function(res) {
+			cb(res);
+		});
+	},
+	update: function(bool, idNum, cb) {
+		orm.updateOne(table, bool, idNum, function(res) {
+			cb(res);
+		});
+	}
+};
+
+// export this file
+module.exports = burger;
