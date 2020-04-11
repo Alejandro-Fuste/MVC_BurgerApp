567dea2a0cbedfac68097ae2e329eec01e66a63a
Alejandro F <afuste7@gmail.com>
2020-04-11 17:56:29 -0400

    changed setting back to local

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
-		host: 'pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		host: 'localhost',
 		port: 3306,
-		user: 'r4gs5dhwo20fmwj2',
-		password: 'ravpn1uk2eofg34x',
-		database: 'aowsui2ev79lc7tv'
+		user: 'root',
+		password: 'yourRootPassword',
+		database: 'burgers_db'
 	});
 }
 
