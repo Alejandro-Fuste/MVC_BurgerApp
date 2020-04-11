851e4aca06146a296880dbaf88f911e75898e7ba
Alejandro F <afuste7@gmail.com>
2020-04-11 17:43:17 -0400

    hardcoded burgers in orm

--- models/burger.js
+++ models/burger.js
@@ -3,21 +3,19 @@ const orm = require('../config/orm');
 
 // code that calls the ORM functions
 
-let table = 'burgers';
-
 const burger = {
 	all: function(cb) {
-		orm.selectAll(table, function(res) {
+		orm.selectAll('burgers', function(res) {
 			cb(res);
 		});
 	},
 	create: function(b_name, dev, cb) {
-		orm.insertOne(table, b_name, dev, function(res) {
+		orm.insertOne('burgers', b_name, dev, function(res) {
 			cb(res);
 		});
 	},
 	update: function(bool, idNum, cb) {
-		orm.updateOne(table, bool, idNum, function(res) {
+		orm.updateOne('burgers', bool, idNum, function(res) {
 			cb(res);
 		});
 	}
