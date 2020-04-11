d94635334c83df2a6f9468561936e7e3b88a8839
Alejandro F <afuste7@gmail.com>
2020-04-11 14:01:33 -0400

    fixed bug with devour buttons, align devour buttons, remove buttons from devoured list, and added gif to readme file

--- README.md
+++ README.md
@@ -70,4 +70,11 @@ npm test
 
 If you have any questions about the repo, open an issue or contact [Alejandro Fuste](https://github.com/ZepCap) directly through GitHub.
 
+## Finished HW GIF
+
+<img src="./public/assets/images/burgerGif.gif" alt="finished gif 1" style="border-radius: 16px" width="300" height="180"/>
+
+
+- - - 
+
 
--- public/assets/js/burgerJquery.js
+++ public/assets/js/burgerJquery.js
@@ -4,48 +4,35 @@ $(function() {
 	$('#create-form').on('submit', (e) => {
 		e.preventDefault();
 
-		console.log('click');
-
 		let newBurger = {
 			burger_name: $('#burg').val().trim(),
 			devoured: $('[name=devouredStatus]:checked').val().trim()
 		};
 
-		console.log(newBurger);
-
 		$.ajax('/api/burgers', {
 			type: 'POST',
 			data: newBurger
 		}).then(() => {
-			console.log('Created new burger!');
-
-			// location.reload();
+			location.reload();
 		});
 	});
 	// Update burger to go to devour list
-	const $devourButton = $('#notDevourList #change-devour.btn.btn-primary');
-	const $devourID = $('button#change-devour.btn.btn-primary');
 
-	$($devourID).on('click', (e) => {
-		let id = $($devourID).data('id');
-		let newDevour = $($devourID).data('devoured');
+	$('.burgerDevour').on('click', function(e) {
+		e.preventDefault();
+
+		let id = $(this).data('id');
 
 		let newDevourState = {
 			id: id,
 			devoured: 1
 		};
 
-		console.log($devourButton);
-		console.log($devourID);
-		console.log(id);
-		console.log(newDevourState);
-
 		$.ajax(`/api/burgers/${id}`, {
 			type: 'PUT',
 			data: newDevourState
 		}).then(() => {
-			console.log(`Changed devoured to ${newDevour}`);
-			// location.reload();
+			location.reload();
 		});
 	});
 });
